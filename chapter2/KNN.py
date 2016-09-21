78414959de1d740f065512a7ac1df619842e5872
MikeForSharing <1356005160@qq.com>
2016-09-21 21:41:38 +0800

    完成KNN算法实例-约会推荐

--- chapter2/KNN.py
+++ chapter2/KNN.py
@@ -50,3 +50,31 @@ def autoNorm(dataSet):
 	NorMat = dataSet - tile(minValue,(m,1))
 	NorMat = NorMat/tile(rangeValue, (m,1) )
 	return NorMat, rangeValue, minValue
+
+
+def datingClassTest():
+	hoRatio = 0.1
+	datingDataMat, datingLabels = file2matrix("datingTestSet.txt")
+	NorMat, rangeVal, minVal = autoNorm(datingDataMat)
+	m = NorMat.shape[0]
+	NumTestVecs = int(m * hoRatio)
+	errorCount = 0.0
+	for i in range(NumTestVecs):
+		classifierResult = classify0(NorMat[i,:],NorMat[NumTestVecs:m,:],datingLabels[NumTestVecs:m],3)
+		print "the classifier come back the result %s, the real result is %s" %(classifierResult,datingLabels[i])
+		if(classifierResult != datingLabels[i]):
+			errorCount += 1.0
+	print "the total error rate is %f" %(errorCount/float(NumTestVecs))
+
+
+
+def classifyPerson():
+	percentGames = float(raw_input("please input the percentage of time to play games"))
+	ffMiles = float(raw_input("please input the frequent fly miles earned per year"))
+	iceLitre = float(raw_input("please input the litre consumed per year"))
+
+	datingDataMat, datingLabels = file2matrix("datingTestSet.txt")
+	norMat, rangeVal, minVal = autoNorm(datingDataMat)
+	itemArr = [ffMiles, percentGames, iceLitre]
+	classifyResult = classify0((itemArr-minVal)/rangeVal, norMat, datingLabels, 3)
+	print "the level you will like this persion is %s" %(classifyResult)
